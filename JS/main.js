d81ce2b6a9cc0966401dbca9c7b143813fbb2523
Yasin Marei <59691385+Yasin3015@users.noreply.github.com>
2023-06-28 00:37:28 +0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -3,3 +3,18 @@ var playButton = document.getElementById("playButton");
 playButton.addEventListener("click", function() {
   window.open("https://www.r4m.co/wp-content/uploads/2021/09/R4_sottotitoli-ENG.mp4?19909", "_blanck");
 });
+
+function myFunction(media) {
+  if (media.matches) { 
+    const navLinks = document.querySelectorAll('.nav-link')
+    navLinks.forEach((l) => {
+        l.onclick = function(){
+          new bootstrap.Collapse(document.getElementById('navbarSupportedContent')).Collapse()
+        }
+    })
+  }
+}
+
+var media = window.matchMedia("(max-width: 992px)")
+myFunction(media)
+media.addListener(myFunction)
